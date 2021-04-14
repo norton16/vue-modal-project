d55aa5427e912a7f7b6f4f5e504a50e6f9ee7526
norton16 <bnorton184@gmail.com>
2021-04-13 23:40:57 -0400

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
+  <div v-if="showModal">
+    <Modal :header="header" :header2="header2" theme="sale"/>
+  </div>
+  <button @click="toggleModal">Show modal</button>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: 'My first Vue App',
+      header: "example prop",
+      header2: [1,2,3],
+      showModal: false
+    }
+  },
+  methods: {
+    handleClick() {
+      // Template ref - take DOM element and store in variable
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="backdrop" @click="closeModal">
+        <!-- Class is sale if theme prop = 'sale' -->
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <h1>Modal Content</h1>
+            <p>{{ header }}</p>
+            <p>{{ header2 }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    // Taking in props
+    props: ['header', 'header2', 'theme'],
+    methods: {
+        closeModal() {
+            // Custom event - listened to from parent component
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+// Scoped - styles only apply to this component
+<style scoped>
+    .modal {
+        width: 400px;
+        padding: 200px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top: 0;
+        left: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    h1 {
+        color: red;
+    }
+    .sale {
+        background: green;
+        color: white;
+    }
+</style>
